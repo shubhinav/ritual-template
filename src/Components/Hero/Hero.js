770ac09c398a41c6e1618cbb0fdd18eeb545c5f3
shubhinav <shubhinavdua@gmail.com>
2021-08-23 19:15:16 +0530

    small warning fix

--- src/Components/Hero/Hero.js
+++ src/Components/Hero/Hero.js
@@ -13,8 +13,8 @@ export default function Hero(){
                 <button className="hero-btn">Shop All</button>
             </div>
             <div className="hero-section-img">
-                <img className="img-desktop" src="//images.ctfassets.net/uuc5ok478nyh/5IMjUB9sibSRJDXXOwa6aV/2477fa4c6b7ed1b755836f0364b8858a/Home-Hero-Desktop.jpg?w=1920&amp;h=747&amp;fl=progressive&amp;q=90&amp;fm=jpg"/>
-                <img className="img-mobile" src="//images.ctfassets.net/uuc5ok478nyh/6dDqtNr4Taao1Qwl4zf6UK/e1141217232b45bdc278746c3a6dcf87/Home-Hero-Mobile.jpg?w=912&amp;h=912&amp;fl=progressive&amp;q=90&amp;fm=jpg"/>
+                <img className="img-desktop" src="//images.ctfassets.net/uuc5ok478nyh/5IMjUB9sibSRJDXXOwa6aV/2477fa4c6b7ed1b755836f0364b8858a/Home-Hero-Desktop.jpg?w=1920&amp;h=747&amp;fl=progressive&amp;q=90&amp;fm=jpg" alt=""/>
+                <img className="img-mobile" src="//images.ctfassets.net/uuc5ok478nyh/6dDqtNr4Taao1Qwl4zf6UK/e1141217232b45bdc278746c3a6dcf87/Home-Hero-Mobile.jpg?w=912&amp;h=912&amp;fl=progressive&amp;q=90&amp;fm=jpg" alt=""/>
             </div>
         </section>
     )
--- src/Components/Main/ProductCard/ProductCard.js
+++ src/Components/Main/ProductCard/ProductCard.js
@@ -4,7 +4,7 @@ export default function ProductCard({text,src}){
     return(
         <div className="product-card">
             <div className="product-card-img">
-                <img src={src}/>
+                <img src={src} alt=""/>
             </div>
             <div className="product-card-name"><span>Shop </span><p>{text}</p></div>
         </div>
